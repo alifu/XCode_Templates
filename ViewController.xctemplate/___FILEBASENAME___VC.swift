17102217290be622029d2f283c4e869bb5d63c9c
alifu <alif@docotel.co.id>
2019-01-09 15:20:53 +0800

    add view controller

---
+++ ViewController.xctemplate/___FILEBASENAME___VC.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: UIViewController  {
+    
+    override func viewDidLoad() {
+        
+        /*
+        Tulis penjelasan fungsi disini
+        */
+        
+        super.viewDidLoad()
+        
+        // Tulis Code Disini
+    }
+    
+}
