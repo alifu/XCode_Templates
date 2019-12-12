1f68ffc20428d9f63553b6669710cd153f8fe1f5
Alif Ramadhoni <alif.bangets@gmail.com>
2019-12-12 19:33:26 +0800

    Update ___FILEBASENAME___Router.swift

--- File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Router.swift
+++ File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Router.swift
@@ -4,12 +4,10 @@ import UIKit
 
 class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouteProtocol {
 
-    class func createModule() -> UIViewController{
-
-        /**
-         * Berfungsi untuk menghubungkan komponen VIPER per Modul
-         */
-
+    /**
+     * Berfungsi untuk menghubungkan komponen VIPER per Modul
+     */
+    class func createModule() -> UIViewController {
         let view = mainstoryboard.instantiateViewController(withIdentifier: "___VARIABLE_ModuleName___VC") as! ___VARIABLE_ModuleName___VC
         let presenter: ___VARIABLE_ModuleName___PresenterProtocol & ___VARIABLE_ModuleName___InteractorOutputProtocol = ___VARIABLE_ModuleName___Presenter()
         let interactor: ___VARIABLE_ModuleName___InteractorInputProtocol = ___VARIABLE_ModuleName___Interactor()
@@ -25,14 +23,12 @@ class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouteProtocol {
         return view
     }
 
+    /**
+     * Berfungsi untuk mengembalikan nilai storyboard
+     * Untuk nama storyboard, dikondisikan
+     */
     static var mainstoryboard: UIStoryboard {
-
-        /**
-         * Berfungsi untuk mengembalikan nilai storyboard
-         * Untuk nama storyboard, dikondisikan
-         */
-
-        return UIStoryboard(name:"Main",bundle: Bundle.main)
+        return UIStoryboard(name:"Main", bundle: Bundle.main)
     }
     
 }
