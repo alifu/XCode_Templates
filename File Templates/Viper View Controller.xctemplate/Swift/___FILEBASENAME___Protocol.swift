c0bc8a50635ed6288fcfbda6e340a6b51377278b
Alif Ramadhoni <alif.bangets@gmail.com>
2019-12-12 19:36:16 +0800

    Update ___FILEBASENAME___Protocol.swift

--- File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Protocol.swift
+++ File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Protocol.swift
@@ -2,37 +2,42 @@
 
 import UIKit
 
+/**
+ * Tambah Method untuk Komunikasi PRESENTER -> VIEW
+ */
 protocol ___VARIABLE_ModuleName___ViewProtocol: class {
     var presenter: ___VARIABLE_ModuleName___PresenterProtocol? { get set }
-    /**
-     * Tambah Method untuk Komunikasi PRESENTER -> VIEW
-     */
+    
 }
 
+/**
+ * Tambah Method untuk Komunikasi PRESENTER -> WIREFRAME
+ */
 protocol ___VARIABLE_ModuleName___RouteProtocol: class {
-    /**
-     * Tambah Method untuk Komunikasi PRESENTER -> WIREFRAME
-     */
+    
 }
 
+/**
+ * Tambah Method untuk Komunikasi VIEW -> PRESENTER
+ */
 protocol ___VARIABLE_ModuleName___PresenterProtocol: class {
     var view: ___VARIABLE_ModuleName___ViewProtocol? { get set }
     var interactor: ___VARIABLE_ModuleName___InteractorInputProtocol? { get set }
     var wireFrame: ___VARIABLE_ModuleName___RouteProtocol? { get set }
-    /**
-     * Tambah Method untuk Komunikasi VIEW -> PRESENTER
-     */
+    
 }
 
+/**
+ * Tambah Method untuk Komunikasi INTERACTOR -> PRESENTER
+ */
 protocol ___VARIABLE_ModuleName___InteractorOutputProtocol: class {
-    /**
-     * Tambah Method untuk Komunikasi INTERACTOR -> PRESENTER
-     */
+    
 }
 
+/**
+ * Tambah Method untuk Komunikasi PRESENTER -> INTERACTOR
+ */
 protocol ___VARIABLE_ModuleName___InteractorInputProtocol: class {
     var presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol? { get set }
-    /**
-     * Tambah Method untuk Komunikasi PRESENTER -> INTERACTOR
-     */
+    
 }
