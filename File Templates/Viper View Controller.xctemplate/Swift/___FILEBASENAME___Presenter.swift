92a7f3d3b9111d1b552dfa264973bac40f7e2bf8
Alif Ramadhoni <alif.bangets@gmail.com>
2021-11-16 23:53:45 +0700

    Update ___FILEBASENAME___Presenter.swift

--- File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Presenter.swift
+++ File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Presenter.swift
@@ -2,18 +2,21 @@
 
 import UIKit
 
-class ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___PresenterProtocol  {
+final class ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___PresenterProtocol  {
     
+    // MARK: - Accessable
+
     weak var view: ___VARIABLE_ModuleName___ViewProtocol?
     var interactor: ___VARIABLE_ModuleName___InteractorInputProtocol?
     var wireFrame: ___VARIABLE_ModuleName___RouteProtocol?
+
+    // MARK: - Private
+
+    // MARK: - Init
     
-    init() {}
-    
+    init() { }
 }
 
-extension ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol {
-    
-    /// Extension INTERACTOR -> PRESENTER
-    
-}
+// MARK: - Extension INTERACTOR -> PRESENTER
+
+extension ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol { }
