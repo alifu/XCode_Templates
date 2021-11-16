6798b1d6e572e9bf67bd5bcaaac7aa7d81baf377
Alif Ramadhoni <alif.bangets@gmail.com>
2021-11-16 23:54:33 +0700

    Update ___FILEBASENAME___Router.swift

--- File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Router.swift
+++ File Templates/Viper View Controller.xctemplate/Swift/___FILEBASENAME___Router.swift
@@ -2,18 +2,17 @@
 
 import UIKit
 
-class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouteProtocol {
+final class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouteProtocol {
 
-    /**
-     * Berfungsi untuk menghubungkan komponen VIPER per Modul
-     */
+    // MARK: - Create Modul
+    
     class func createModule() -> UIViewController {
         let view = mainstoryboard.instantiateViewController(withIdentifier: "___VARIABLE_ModuleName___VC") as! ___VARIABLE_ModuleName___VC
         let presenter: ___VARIABLE_ModuleName___PresenterProtocol & ___VARIABLE_ModuleName___InteractorOutputProtocol = ___VARIABLE_ModuleName___Presenter()
         let interactor: ___VARIABLE_ModuleName___InteractorInputProtocol = ___VARIABLE_ModuleName___Interactor()
         let router: ___VARIABLE_ModuleName___RouteProtocol = ___VARIABLE_ModuleName___Router()
 
-        /// Conecting
+        // Conecting
         view.presenter = presenter
         presenter.view = view
         presenter.wireFrame = router
@@ -23,13 +22,9 @@ class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouteProtocol {
         return view
     }
 
-    /**
-     * Berfungsi untuk mengembalikan nilai storyboard
-     * Untuk nama storyboard, dikondisikan
-     */
+    // MARK: - Static variable for storyboard
+
     static var mainstoryboard: UIStoryboard {
         return UIStoryboard(name:"Main", bundle: Bundle.main)
     }
-    
 }
-
